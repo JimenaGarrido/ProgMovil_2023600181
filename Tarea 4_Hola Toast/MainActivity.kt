9c3a673d9b3bbd27bccd0f16080df30be3235822
JimenaGarrido <jimenagarrido332@gmail.com>
2025-03-07 17:50:48 -0600

    Quitar libreria de MainActivity.kt

--- Tarea 4_Hola Toast/MainActivity.kt
+++ Tarea 4_Hola Toast/MainActivity.kt
@@ -1,35 +1,3 @@
-package com.example.prueba
-
-import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.provider.FontsContractCompat.Columns
-import com.example.prueba.ui.theme.PruebaTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //Constructor de la clase padre
